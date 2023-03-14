854c55cedf9fb8980cf2d8f486242cef2f6ff969
Adam Shaw <47095718+AdamShawDEV@users.noreply.github.com>
2023-03-14 12:03:57 +0700

    added user image support

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,73 +1,97 @@
-import styles from './modules/Header.module.css';
-import logo from './images/logo.png';
+import styles from "./modules/Header.module.css";
+import logo from "./images/logo.png";
 // import userImage from './images/user_pic.jpg';
-import { useContext, useEffect, useState } from 'react';
-import { NoteFilterContext } from './hooks/NoteFilterContext';
-import { RiCloseLine } from 'react-icons/ri';
-import { AiOutlineSearch } from 'react-icons/ai';
-import Button from './Button';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, db, logout } from '../firebase';
-import { MODAL_OPEN } from '../consts';
-import { doc, getDoc } from 'firebase/firestore';
+import { useContext, useEffect, useState } from "react";
+import { NoteFilterContext } from "./hooks/NoteFilterContext";
+import { RiCloseLine } from "react-icons/ri";
+import { AiOutlineSearch } from "react-icons/ai";
+import Button from "./Button";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logout } from "../firebase";
+import { MODAL_OPEN } from "../consts";
+import { doc, getDoc } from "firebase/firestore";
 
 function Header({ setModalOpen }) {
-    const { noteFilter, setNoteFilter } = useContext(NoteFilterContext);
-    const [user] = useAuthState(auth);
-    const [userImage, setUserImage] = useState(null);
+  const { noteFilter, setNoteFilter } = useContext(NoteFilterContext);
+  const [user] = useAuthState(auth);
+  const [userImage, setUserImage] = useState(null);
 
-    useEffect(() => {
-        const getUserData = async () => {
-            if (user) {
-                const docRef = doc(db, 'users', user.uid);
-                const docSnap = await getDoc(docRef);
-                const userData = docSnap.data();
-                if (userData.authProvider === 'google') {
-                    setUserImage(user.photoURL);
-                } else if (userData.authProvider === 'local') {
-                    user.displayName = userData.name;
-                    setUserImage(process.env.PUBLIC_URL + `/images/${userData.userImage}.png`);
-                }
-            } else {
-                setUserImage(null);
-            }
+  useEffect(() => {
+    const getUserData = async () => {
+      if (user) {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        const userData = docSnap.data();
+        if (userData.authProvider === "google") {
+          setUserImage(user.photoURL);
+        } else if (userData.authProvider === "local") {
+          user.displayName = userData.name;
+          setUserImage(
+            process.env.PUBLIC_URL + `/images/${userData.userImage}.png`
+          );
         }
+      } else {
+        setUserImage(null);
+      }
+    };
 
-        getUserData();
-    }, [user])
+    getUserData();
+  }, [user]);
 
-    return (
-        <>
-            <header className={styles.header}>
-                <div className={styles.logo} >
-                    <img src={logo} alt='notify-me' />
-                </div>
-                <div className={styles.filterContainer}>
-                    <div className={styles.filterButtonContainer}>
-                        <AiOutlineSearch style={{ position: 'absolute', left: '.4rem', top: '.25rem', fontSize: '1.5rem', color: '#696969ff' }} />
-                        <input className={styles.filterBox}
-                            type='text'
-                            placeholder='search notes'
-                            onChange={e => setNoteFilter(e.target.value)}
-                            value={noteFilter} />
-                        {noteFilter && <button title='clear' onClick={() => setNoteFilter('')} class={styles.clearButton}><RiCloseLine /></button>}
-                    </div>
-                </div>
-                <div className={styles.userContainer} >
-                    {/* <span className={styles.userName}>Adam Shaw</span>
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="notify-me" />
+        </div>
+        <div className={styles.filterContainer}>
+          <div className={styles.filterButtonContainer}>
+            <AiOutlineSearch
+              style={{
+                position: "absolute",
+                left: ".4rem",
+                top: ".25rem",
+                fontSize: "1.5rem",
+                color: "#696969ff",
+              }}
+            />
+            <input
+              className={styles.filterBox}
+              type="text"
+              placeholder="search notes"
+              onChange={(e) => setNoteFilter(e.target.value)}
+              value={noteFilter}
+            />
+            {noteFilter && (
+              <button
+                title="clear"
+                onClick={() => setNoteFilter("")}
+                class={styles.clearButton}
+              >
+                <RiCloseLine />
+              </button>
+            )}
+          </div>
+        </div>
+        <div className={styles.userContainer}>
+          {/* <span className={styles.userName}>Adam Shaw</span>
                 <img className={styles.userImage} alt='user pic' src={userImage} /> */}
-                    {user ?
-                        <button className={styles.userName} onClick={logout}>
-                            {user.displayName}
-                        </button> :
-                        <Button onClick={() => setModalOpen(MODAL_OPEN.LOGIN)}>
-                            login
-                        </Button>}
-                    {userImage && <img className={styles.userImage} alt='user pic' src={userImage} />}
-                </div>
-            </header>
-        </>
-    );
+          {user ? (
+            <button className={styles.userName} onClick={logout}>
+              {user.displayName}
+            </button>
+          ) : (
+            <Button onClick={() => setModalOpen(MODAL_OPEN.LOGIN)}>
+              login
+            </Button>
+          )}
+          {userImage && (
+            <img className={styles.userImage} alt="user pic" src={userImage} />
+          )}
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
